d30a0bb9d64a8d32b5930a916b2581f7adccf19d
slaitila <sebastian.laitila@cls-engineering.fi>
2016-09-26 14:27:54 +0300

    taidetta

--- ng-app.js
+++ ng-app.js
@@ -6,4 +6,27 @@ app.controller('main', function($scope)
   {
       endPosition = position;
   }
+
+  loadObject("outrun");
+
+  function loadObject(objectName)
+  {
+    var jsonload = new THREE.ObjectLoader();
+    jsonload.load("gallery/"+objectName+".json",
+    function(object)
+    {
+      object.position.x = 1200
+      object.position.y= 100;
+      object.position.z= 900;
+      console.log(object.position)
+      object.scale.x = 10
+      object.scale.y = 10
+      object.scale.z = 10
+      //console.log(geometry);
+      //var mater = new THREE.MultiMaterial(materials);
+
+
+      scene.add(object);
+    });
+}
 })
